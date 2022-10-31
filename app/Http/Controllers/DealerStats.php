427ad545ad279b733c0967a35684671b769dd993
Hung Do <hung.do@traderinteractive.com>
2022-10-31 18:28:38 -0400

    Using a test to verify the combine logic

--- app/Http/Controllers/DealerStats.php
+++ app/Http/Controllers/DealerStats.php
@@ -15,11 +15,95 @@ class DealerStats extends Controller
      */
     public function index()
     {
-        return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA',
-        ]);
+        $res1 = [
+            "classCounts" => [
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "1",
+                    "totalCount" => "10"
+                ],
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "2",
+                    "totalCount" => "11"
+                ],
+            ],
+            "makeCounts" => [
+                [
+                    "makeName" => "1954 MFG",
+                    "makeId" => "765341026",
+                    "totalCount" => "3"
+                ]
+            ]
+        ];
+
+        $res2 = [
+            "classCounts" => [
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "1",
+                    "totalCount" => "40"
+                ],
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "3",
+                    "totalCount" => "90"
+                ]
+            ]
+        ];
+
+        $res3 = [
+            "modelCounts" => [
+                [
+                    "modelName" => "108 SD",
+                    "modelId" => "764939214",
+                    "totalCount" => "1"
+                ],
+            ]
+        ];
+
+        $responses = [
+            $res1,
+            $res2,
+            $res3
+        ];
+
+        $filters = [
+            "classCounts" => "classId",
+            "makeCounts" => "makeId",
+            "modelCounts" => "modelId"
+        ];
+
+        $result = [];
+
+        foreach ($responses as $response) {
+            foreach ($filters as $countName => $filterName) {
+                $elements = $response[$countName] ?? [];
+                foreach ($elements as $element) {
+                    $result[$countName] = $this->_addElement($result[$countName] ?? [], $element, $filterName);
+                }
+            }
+        }
+
+        return response()->json($result);
     }
 
-    
+    private function _addElement(array $array, array $elementToAdd, string $filterName)
+    {
+        if (empty($array)) {
+            return [$elementToAdd];
+        }
+
+        $result = [];
+        foreach ($array as $element) {
+            if ($element[$filterName] == $elementToAdd[$filterName]) {
+                $elementToAdd['totalCount'] += $element['totalCount'];
+            } else {
+                $result[] = $element;
+            }
+        }
+        $result[] = $elementToAdd;
+
+        return $result;
+    }
 }
--- tests/Feature/DealerStatsTest.php
+++ tests/Feature/DealerStatsTest.php
@@ -16,10 +16,46 @@ class DealerStatsTest extends TestCase
 
         // Act
         $response = $this->get('/api/dealer-stats');
+        $result = json_decode($response->getContent(), true);
+
+        //Expect
+        $expect = [
+            "classCounts" => [
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "2",
+                    "totalCount" => "11"
+                ],
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "1",
+                    "totalCount" => "50"
+                ],
+                [
+                    "className" => "CLASS 1 (GVW 0 - 6000)",
+                    "classId" => "3",
+                    "totalCount" => "90"
+                ],
+            ],
+            "makeCounts" => [
+                [
+                    "makeName" => "1954 MFG",
+                    "makeId" => "765341026",
+                    "totalCount" => "3"
+                ]
+                ],
+            "modelCounts" => [
+                [
+                    "modelName" => "108 SD",
+                    "modelId" => "764939214",
+                    "totalCount" => "1"
+                ],
+            ]
+        ];
 
         //Assert
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
+        $this->assertEquals($result, $expect);
 
-            
     }
 }
